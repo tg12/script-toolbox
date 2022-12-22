a0e4326646fb83dd950c0bd83b1eb51168c435c2
tg12 <tg12@users.noreply.github.com>
2022-12-22 16:32:42 +0000

    Create setup_docker.sh

---
+++ setup_docker.sh
@@ -0,0 +1,11 @@
+sudo apt-get update
+
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
